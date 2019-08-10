6baa99f07d804d6c4559afbbe94f7ee7b0730de3
andriy.klymenko <andriy.klymenko@sigma.software>
2019-08-10 14:37:38 +0300

    020: Add link to task.

--- src/main/java/com/sigmaukraine/klymenko/andriy/lists/reverse/Solution.java
+++ src/main/java/com/sigmaukraine/klymenko/andriy/lists/reverse/Solution.java
@@ -1,5 +1,8 @@
 package com.sigmaukraine.klymenko.andriy.lists.reverse;
 
+/**
+ * Solution for <a href="https://www.hackerrank.com/challenges/insert-a-node-into-a-sorted-doubly-linked-list/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=linked-lists">Inserting a Node Into a Sorted Doubly Linked List</a>.
+ */
 public class Solution {
 
     static class DoublyLinkedListNode {
